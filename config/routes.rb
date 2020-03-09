52f5df4b7740cdf3f22e8a7033ad8a5f3a77916f
rmiri <re_miriuk@hotmail.com>
2020-03-09 18:11:32 +0000

    auth login created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'auth/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create]
+  post '/login', to: 'autho#create'
+  get '/profile', to: 'users#profile'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create(
+    email: "email@fakeemail.com",
+    password: "123",
+    admin?: true,
+    name: "Renata",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/4/49/Syvia_of_Sylvia%27s_reaturant_N.Y.C_%28cropped%29.jpg"
+)

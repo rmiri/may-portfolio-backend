1998cd59dd21a045a7222d878aca1fe83b964cc7
rmiri <re_miriuk@hotmail.com>
2020-03-13 14:39:11 +0000

    project posting working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,19 @@
 class ProjectsController < ApplicationController
+    skip_before_action :authorized
     def index
         render json: Project.all
     end
     def show
         render json: Project.find(params[:id])
+    end
+    def create
+  
+        @project = Project.new(title: params[:title], index_description: params[:index_description], description: params[:description], category: params[:category], user_id: params[:user_id]  )
+    
+        if @project.save
+            render json: {message: "all good msiter"}
+        else
+            render json: {error: "something went wrong, try again later"}
+        end
     end
 end

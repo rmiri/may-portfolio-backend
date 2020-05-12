d74ebba60981f40090e52f4e96b9fa5fb293555f
github username <github email address>
2020-05-12 16:30:14 +0100

    changed database.yml production to url

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,13 +2,23 @@ class ProjectsController < ApplicationController
     skip_before_action :authorized
     def index
         @projectsWithPics = Project.all.map{ |proj| {
-            project: proj, pictures: url_for(proj.pictures)} }
+            project: proj, 
+            pictures: url_for(proj.pictures),
+            } }
         render json: @projectsWithPics
     end
     def show
+        # debugger
         @project = Project.find(params[:id])
+        if @project.photos 
+             myPhotos = @project.photos.map { |photo| url_for(photo)}
+        
+        else
+            myPhotos = ["#"]
+        end
+
         if @project.pictures
-            render json: {project: @project, pictures: url_for(@project.pictures)}
+            render json: {project: @project, pictures: url_for(@project.pictures), photos: myPhotos}
         else
             render json: {project: @project, pictures: "#"}
         end
@@ -31,7 +41,7 @@ class ProjectsController < ApplicationController
     private
 
     def project_params
-        params.permit(:title, :index_description, :description, :category, :user_id, :pictures)
+        params.permit(:title, :index_description, :description, :category, :user_id, :pictures, :photos)
     end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,30 @@
 class Project < ApplicationRecord
+    include Rails.application.routes.url_helpers
+
     belongs_to :user
     has_one_attached :pictures
+    has_many_attached :photos
+
+
+  
+    def my_url
+      rails_blob_path(self.photos, disposition: "attachment")
+    end
 
 
     def allPictures
-        self.pictures.map
+        url_for(self.pictures)
+    end
+
+    def allPhotos
+        if self.photos
+            allPhotos = self.photos
+            photos.map { |photo|
+            url_for(photo)
+        }
+        else 
+            photos = ["#"]
+        end
     end
 
 end
